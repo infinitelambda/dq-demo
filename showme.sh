8555384163a8516ca0a6a6da9f18464eb4b4ddea
Dat Nguyen <dat@infinitelambda>
2023-04-15 19:25:39 +0700

    add showme.sh

--- showme.sh
+++ showme.sh
@@ -1,9 +1,11 @@
-# 1. transform and test data
+# 1 & 2. Run: transform, test and capture test results
 cd dbt
 dbt build --exclude package:dq_tools --vars '{dq_tools_enable_store_test_results: true}'
-dbt build -s package:dq_tools
 
-# 2. run data quality dashboard
+# 3. Build mart of Data Quality
+dbt build -s package:dq_tools
 cd ..
 python dashboard/jobs/export_dq_marts.py
+
+# 4. Get the dashboard
 streamlit run dashboard/data_quality_score.py
